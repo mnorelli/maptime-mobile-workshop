70b85ead3701735e4d7b74426f2f79034cf13a6d
langsmith <langstonlmcs@gmail.com>
2019-04-16 12:05:10 -0700

    more adjustments

--- android/maptime-mobile/app/src/main/java/com/mapbox/maptime_mobile/TaskThreeActivity.java
+++ android/maptime-mobile/app/src/main/java/com/mapbox/maptime_mobile/TaskThreeActivity.java
@@ -34,16 +34,19 @@ public class TaskThreeActivity extends AppCompatActivity implements MapView.OnDi
       @Override
       public void onMapReady(@NonNull final MapboxMap mapboxMap) {
 
-        mapboxMap.setStyle(Style.MAPBOX_STREETS,
-
-
+        mapboxMap.setStyle(Style.DARK,
           new Style.OnStyleLoaded() {
             @Override
             public void onStyleLoaded(@NonNull Style style) {
 
               // 🔥 Hot tip: Change the water color at runtime!
               // The map style being used already has its water color defined. But, we can change
-              // it at runtime through this code. This is called runtime styling, since we are changing
+              // it at runtime through this code.
+              //
+              // Run the app to see the custom rgb color used for the water color. Then delete
+              // this hot tip's code below and re-run the app to see the original water color.
+              //
+              // This is called runtime styling, since we are changing
               // the map's style at runtime. To change the water color, we need to retrieve the layer
               // containing the water data and then we set the new color of the water.
               // The returned result of any attempt to retrieve a layer in a map is an optional value that may
@@ -53,9 +56,8 @@ public class TaskThreeActivity extends AppCompatActivity implements MapView.OnDi
 
               if (style.getLayer("water") != null) {
                 Layer waterLayer = style.getLayer("water");
-                waterLayer.setProperties(fillColor(rgba(0, 0.58, 1, 1)));
+                waterLayer.setProperties(fillColor(rgba(4, 147, 255, 1)));
               }
-
 
               // ⭐️ Challenge: Customize color of the country borders
               // Using the above as a reference, can you change the color of the country borders?
@@ -70,29 +72,39 @@ public class TaskThreeActivity extends AppCompatActivity implements MapView.OnDi
               // One of the most powerful features of mobile mapping is the ability to read data sources from
               // somewhere else and add them to a map! Below is a URL to a GeoJSON feed of earthquakes recorded
               // by the USGS within the past week. In this challenge, you should:
-              // • Register a new data source with the map by creating a new MGLShapeSource with the above URL
+              // • Register a new data source with the map by creating a new GeoJsonSource with the above URL
               //   API reference: https://docs.mapbox.com/android/api/map-sdk/7.3.0/com/mapbox/mapboxsdk/style/sources/GeoJsonSource.html
+              // • Add the new data source to the map's style
+              //   API reference: https://docs.mapbox.com/android/api/map-sdk/7.3.0/com/mapbox/mapboxsdk/maps/Style.html
               // • Create a new style layer to visualize the data source using a CircleLayer
               //   API reference: https://docs.mapbox.com/android/api/map-sdk/7.3.0/com/mapbox/mapboxsdk/style/layers/CircleLayer.html
-              // • Add the new data source and style layer to the map's style
+              //   Example: https://docs.mapbox.com/android/maps/examples/style-circles-categorically
+              // • Add the new CircleLayer to the map's style
               //   API reference: https://docs.mapbox.com/android/api/map-sdk/7.3.0/com/mapbox/mapboxsdk/maps/Style.html
 
+              String earthquakeGeoJsonUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-              // ⭐️ Challenge: Style each circle based off of an earthquake's intensity
-              // Once you have completed the above challenge, you will see black circles on your map
-              // representing the locations of earthquakes that have occurred within the past week.
-              // To better visualize this data, you will now apply a different color to each circle based
-              // off its intensity. In this challenge, you should:
+              // ⭐️ Challenge: Style each circle based off of an earthquake's intensity once you have
+              // completed the above challenge. Unless you already set a custom color,
+              // you will see black circles on your map representing the locations of earthquakes that
+              // have occurred within the past week. To better visualize this data, you will now apply a
+              // different color to each circle based off its intensity. In this challenge, you should:
               // • Use the below dictionary of stop values to set the layer's circle color based on the
               //   severity of the earthquake, using the data source's "mag" field which represents its magnitude.
-              //   Example: https://docs.mapbox.com/android/maps/examples/style-circles-categorically/
+
+              // Use can use https://htmlcolorcodes.com/color-picker if you'd like to find an exact color
+              // for a data "stop".
+
+              //   Examples: https://docs.mapbox.com/android/maps/examples/style-circles-categorically
+              //             https://docs.mapbox.com/android/maps/examples/circle-layer-clustering
 
               Expression.Stop[] stopArray = new Expression.Stop[] {
-                stop(0, Color.GREEN),
-                stop(2, Color.YELLOW),
-                stop(5, Color.MAGENTA),
-                stop(10, Color.RED),
+                stop(0, Expression.color(Color.GREEN)),
+                stop(.8, Expression.color(Color.parseColor("#FF00D5"))), // hot pink
+                stop(2, Expression.color(Color.parseColor("#FF8A00"))), // bright orange
+                stop(3, Expression.color(Color.RED)),
               };
+
 
             }
           });
